9656bd101d80f0d7b4a4165127be16fd0a744a26
Bryan Avalos Loa y Pardo <bryanavalossistemas@gmail.com>
2025-03-05 03:19:26 -0500

    second commit

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import morgan from 'morgan';
+import * as morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
